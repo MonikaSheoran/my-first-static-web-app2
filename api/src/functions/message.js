3e55535dd05117a48ff6e1bf5cb4adba615ee79a
Larry Welch <norman.welch@slalom.com>
2025-07-01 16:11:11 -0600

    Changes for review

--- api/src/functions/message.js
+++ api/src/functions/message.js
@@ -1,5 +1,8 @@
 const { app } = require('@azure/functions');
+const { BlobServiceClient } = require('@azure/storage-blob');
+const multipart = require('parse-multipart');
 
+// Message function (v4 model)
 app.http('message', {
     methods: ['GET', 'POST'],
     authLevel: 'anonymous',
--- api/src/functions/storage.js
+++ api/src/functions/storage.js
@@ -2,15 +2,6 @@ const { app } = require('@azure/functions');
 const { BlobServiceClient } = require('@azure/storage-blob');
 const multipart = require('parse-multipart');
 
-// Message function (v4 model)
-app.http('message', {
-    methods: ['GET', 'POST'],
-    authLevel: 'anonymous',
-    handler: async (request, context) => {
-        return { body: `Hello, from the API!` };
-    }
-});
-
 // Storage function (v4 model)
 app.http('storage', {
     methods: ['POST'],
